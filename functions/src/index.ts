68baf74cf1d7631eded1139766e2a38518d2ba00
Phaedrus Raznikov <phaedrus.raznikov@memre.ai>
2023-04-13 08:04:05 -0500

    Take fuller message history for context

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,28 +1,42 @@
 import * as functions from "firebase-functions";
-import { Configuration,OpenAIApi } from "openai";
+import { Configuration, OpenAIApi } from "openai";
 
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
 const openai = new OpenAIApi(configuration);
 
-// // Start writing functions
-// // https://firebase.google.com/docs/functions/typescript
-export const helloWorld = functions.https.onRequest((_request, response) => {
-  functions.logger.info("Hello logs!", { structuredData: true });
-  response.send("Hello from Firebase!");
-});
-
-// TODO: Accept multiple messages in the request body
-// to handle fuller conversations before falling back on memory
 export const getResponseToMessage = functions
   .runWith({ timeoutSeconds: 540 })
   .https.onRequest(async (request, response) => {
     try {
-      const { message } = request.body;
-      if (!message) {
-        response.status(400).send({ error: "Message is required" });
+      const { messages } = request.body;
+      if (!messages) {
+        response.status(400).send({ error: "Messages is required" });
       }
+
+      // Validate that messages is an array of objects with a role and content
+      if (!Array.isArray(messages)) {
+        response.status(400).send({ error: "Messages must be an array" });
+        return;
+      }
+
+      for (const message of messages) {
+        if (
+          typeof message !== "object" ||
+          !message.role ||
+          !message.content ||
+          typeof message.role !== "string" ||
+          typeof message.content !== "string"
+        ) {
+          response
+            .status(400)
+            .send({ error: "Messages must be an array of objects" });
+          return;
+        }
+      }
+
+      // TODO: Truncate total length of combined messages and system init to 2048 tokens
 
       const openaiResponse = await openai.createChatCompletion({
         model: "gpt-3.5-turbo",
@@ -36,14 +50,12 @@ export const getResponseToMessage = functions
             content: `Instructions:
 - You are a friendly AI named Judith
 - You are encouraging, supportive, and helpful
-- You are a world-class psychologist and expert in cognitive behavioral therapy
-- You are a world-class life coach
-- You behave as a friend, advisor, mentor, confidant, and cognitive behavioral therapist`,
+- You are a world-class therapist and expert in cognitive behavioral therapy
+- You act primarily as a friend, and your comments are generally casual and conversational
+- You are a good listener and you are patient
+- Your responses typically mirror the length of the user's input`,
           },
-          {
-            role: "user",
-            content: message,
-          },
+          ...messages,
         ],
       });
 
@@ -54,12 +66,10 @@ export const getResponseToMessage = functions
         return;
       }
 
-      response
-        .status(200)
-        .send({
-          response:
-            openaiResponse.data.choices[0].message?.content.trim() || null,
-        });
+      response.status(200).send({
+        response:
+          openaiResponse.data.choices[0].message?.content.trim() || null,
+      });
     } catch (err: any) {
       console.error(err);
       functions.logger.error(err);
