828cd6045fc05522ccdb854b95e8c240bcd703cb
Phaedrus Raznikov <phaedrus.raznikov@memre.ai>
2023-04-15 09:13:10 -0500

    Handle staging and prod environments

--- functions/src/firebase.ts
+++ functions/src/firebase.ts
@@ -1,8 +1,14 @@
 import * as firebaseAdmin from "firebase-admin";
-import serviceAccount from "../service-account-key.json";
+import prodServiceAccount from "../service-account-key-prod.json";
+import stagingServiceAccount from "../service-account-key-staging.json";
+
+const serviceAccount =
+  process.env.NODE_ENV === "production"
+    ? prodServiceAccount
+    : stagingServiceAccount;
 
 firebaseAdmin.initializeApp({
-  credential: firebaseAdmin.credential.cert(serviceAccount as any)
+  credential: firebaseAdmin.credential.cert(serviceAccount as any),
 });
 
 export const admin = firebaseAdmin;
